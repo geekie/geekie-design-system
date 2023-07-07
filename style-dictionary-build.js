d1cd91b48a7e1066696ae30a52f8b3b3af8d49ae
Gabriel Guilhoto <gabriel.guilhoto@geekie.com.br>
2023-07-07 17:02:05 -0300

    tentativa de configuração da extensão de design tokens

---
+++ style-dictionary-build.js
@@ -0,0 +1,62 @@
+const StyleDictionary = require("style-dictionary");
+
+// these are the tokens we have in our stylesheet
+const DESIGN_TOKEN_TYPES = [
+  "color",
+  "form",
+  "animation",
+  "font",
+  "text",
+  "breakpoint",
+];
+
+// just get the token name from it, like: color
+const extractTokenNameFromDictionaryName = (variable) => {
+  if (variable) {
+    const [, name] = variable.match(/([^-]+)/);
+    return name;
+  }
+};
+
+// Capitalize first letter to respect the addon parser for finding the right Presenter
+const sanitizeString = (string) => {
+  return string.charAt(0).toUpperCase() + string.slice(1);
+};
+
+// Register your own format
+StyleDictionary.registerFormat({
+  name: `scss/variables-with-headers`,
+  formatter: function ({ dictionary, file }) {
+    return (
+      StyleDictionary.formatHelpers.fileHeader({ file }) +
+      ":root {\n" +
+      DESIGN_TOKEN_TYPES.map(
+        (item) =>
+          `\n/**
+* @tokens ${sanitizeString(item)}s
+* @presenter ${sanitizeString(item)}
+*/\n` +
+          dictionary.allTokens
+            .filter(
+              (token) => item === extractTokenNameFromDictionaryName(token.name)
+            )
+            .map((token) => `--${token.name}: ${token.value};`)
+            .join("\n")
+      ).join("\n") +
+      "}\n"
+    );
+  },
+});
+
+// APPLY THE CONFIGURATION
+// IMPORTANT: the registration of custom transforms
+// needs to be done _before_ applying the configuration
+const StyleDictionaryExtended = StyleDictionary.extend(
+  __dirname + "/style-dictionary-config.json"
+);
+
+// FINALLY, BUILD ALL THE PLATFORMS
+StyleDictionaryExtended.buildAllPlatforms();
+
+console.log("\n==============================================");
+console.log("\nBuild completed!");
