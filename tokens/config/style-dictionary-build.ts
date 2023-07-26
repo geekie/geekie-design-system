ede108111fe7b0b90b636e3a2164fe1ec4834e26
gabrielguilhoto <gabriel.guilhoto@geekie.com.br>
2023-07-26 16:51:38 -0300

    Corrige problemas de lint no style-dictionary-build e no CustomDesignTokenDocBlock (#11)
    Esta é uma continuação do https://github.com/projetoeureka/geekie-design-system/pull/10. Esse PR configurou o lint no repositório, mas havia dois arquivos (`tokens/config/style-dictionary-build.js` e `utils/CustomDesignTokenDocBlock/index.tsx`) que precisavam de correções de lint mais manuais. Preferi deixar elas num PR à parte.
    
    O `tokens/config/style-dictionary-build.js` era um arquivo JS, mas a configuração do ESLint supunha que deveriam ser aplicadas regras de TS a ele. Achei que era melhor converter o arquivo para TS do que mudar a configuração do ESLint para tratar arquivos JS diferentemente. Para ser possível rodar esse arquivo da forma como era antes, instalei também o `ts-node`.

---
+++ tokens/config/style-dictionary-build.ts
@@ -0,0 +1,100 @@
+import path from 'path';
+import StyleDictionary from 'style-dictionary';
+import type { Dictionary } from 'style-dictionary/types/Dictionary';
+
+// The storybook-design-token plugin expects the tokens to be split in categories separated by
+// headers on the SCSS file, so we generate it accordingly. The code based on the suggestion from
+// https://github.com/amzn/style-dictionary/issues/344#issuecomment-1200826141.
+const DESIGN_TOKEN_CATEGORIES_BY_PREFIX = {
+  DSA_COLOR_BRAND: { categoryName: 'Brand Colors', presenterName: 'Color' },
+  DSA_COLOR_BUTTON: { categoryName: 'Button Colors', presenterName: 'Color' },
+  DSA_COLOR_NEUTRAL: { categoryName: 'Neutral Colors', presenterName: 'Color' },
+  DSA_COLOR_FEEDBACK: {
+    categoryName: 'Feedback Colors',
+    presenterName: 'Color',
+  },
+  DSA_COLOR_MATERIAS: {
+    categoryName: 'Materias Colors',
+    presenterName: 'Color',
+  },
+  DSA_COLOR_HIGHLIGHTS: {
+    categoryName: 'Highlights Colors',
+    presenterName: 'Color',
+  },
+  DSA_FONT_FAMILY: {
+    categoryName: 'Font families',
+    presenterName: 'FontFamily',
+  },
+  DSA_FONT_SIZE: { categoryName: 'Font sizes', presenterName: 'FontSize' },
+  DSA_FONT_WEIGHT: {
+    categoryName: 'Font weights',
+    presenterName: 'FontWeight',
+  },
+  DSA_LINE_HEIGHT: {
+    categoryName: 'Line heights',
+    presenterName: 'LineHeight',
+  },
+};
+type TokenCategoryPrefix = keyof typeof DESIGN_TOKEN_CATEGORIES_BY_PREFIX;
+
+const extractTokenCategoryPrefix = (
+  tokenName: string
+): TokenCategoryPrefix | undefined => {
+  return (
+    Object.keys(DESIGN_TOKEN_CATEGORIES_BY_PREFIX) as TokenCategoryPrefix[]
+  ).find((prefix) => tokenName.startsWith(prefix));
+};
+
+const formatCategory = ({ dictionary }: { dictionary: Dictionary }): string[] =>
+  Object.entries(DESIGN_TOKEN_CATEGORIES_BY_PREFIX).map(
+    ([prefix, names]) =>
+      `\n/**
+* @tokens ${names.categoryName}
+* @presenter ${names.presenterName}
+*/\n` +
+      dictionary.allTokens
+        .filter((token) => prefix === extractTokenCategoryPrefix(token.name))
+        .map((token) => {
+          const tokenCategory = extractTokenCategoryPrefix(token.name);
+          const unit = tokenCategory === 'DSA_FONT_SIZE' ? 'px' : '';
+          return `$${token.name}: ${token.value as number}${unit};`;
+        })
+        .join('\n')
+  );
+
+StyleDictionary.registerFormat({
+  name: `scss/variables-with-headers`,
+  formatter: function ({ dictionary, file }) {
+    return (
+      StyleDictionary.formatHelpers.fileHeader({ file }) +
+      ':root {' +
+      formatCategory({ dictionary }).join('\n') +
+      '\n}\n'
+    );
+  },
+});
+
+StyleDictionary.registerTransformGroup({
+  name: 'custom/js',
+  transforms: StyleDictionary.transformGroup.js
+    .filter((transform) => transform !== 'color/hex')
+    .concat(['color/css', 'name/cti/constant']),
+});
+
+StyleDictionary.registerTransformGroup({
+  name: 'custom/scss',
+  transforms: StyleDictionary.transformGroup.scss.concat(['name/cti/constant']),
+});
+
+// APPLY THE CONFIGURATION
+// IMPORTANT: the registration of custom transforms
+// needs to be done _before_ applying the configuration
+const StyleDictionaryExtended = StyleDictionary.extend(
+  path.join(__dirname, '/style-dictionary-config.json')
+);
+
+// FINALLY, BUILD ALL THE PLATFORMS
+StyleDictionaryExtended.buildAllPlatforms();
+
+console.log('\n==============================================');
+console.log('\nBuild completed!');
