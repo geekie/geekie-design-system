78540b04b8a079aaf26d3d01f2e7f64f880ffe0b
Gabriel Guilhoto <gabriel.guilhoto@geekie.com.br>
2023-08-18 10:45:02 -0300

    presets ludic e reading

---
+++ token-presets/reading.ts
@@ -0,0 +1,153 @@
+import {
+  DSA_FONT_FAMILY_READING,
+  DSA_FONT_SIZE_L,
+  DSA_FONT_SIZE_M,
+  DSA_FONT_SIZE_S,
+  DSA_FONT_SIZE_XS,
+  DSA_FONT_STYLE_ITALIC,
+  DSA_FONT_WEIGHT_BOLD,
+  DSA_FONT_WEIGHT_NORMAL,
+  DSA_LETTER_SPACING_NORMAL,
+  DSA_LINE_HEIGHT_M,
+  DSA_LINE_HEIGHT_S,
+} from '../build/tokens/ts/tokens';
+
+/* Body (large) */
+export const DSA_READING_BODY_L_NORMAL_STYLE = {
+  fontFamily: DSA_FONT_FAMILY_READING,
+  fontSize: DSA_FONT_SIZE_L,
+  fontWeight: DSA_FONT_WEIGHT_NORMAL,
+  letterSpacing: DSA_LETTER_SPACING_NORMAL,
+  lineHeight: DSA_LINE_HEIGHT_M,
+};
+
+export const DSA_READING_BODY_L_BOLD_STYLE = {
+  fontFamily: DSA_FONT_FAMILY_READING,
+  fontSize: DSA_FONT_SIZE_L,
+  fontWeight: DSA_FONT_WEIGHT_BOLD,
+  letterSpacing: DSA_LETTER_SPACING_NORMAL,
+  lineHeight: DSA_LINE_HEIGHT_M,
+};
+
+export const DSA_READING_BODY_L_ITALIC_STYLE = {
+  fontFamily: DSA_FONT_FAMILY_READING,
+  fontSize: DSA_FONT_SIZE_L,
+  fontStyle: DSA_FONT_STYLE_ITALIC,
+  fontWeight: DSA_FONT_WEIGHT_NORMAL,
+  letterSpacing: DSA_LETTER_SPACING_NORMAL,
+  lineHeight: DSA_LINE_HEIGHT_M,
+};
+
+export const DSA_READING_BODY_L_BOLDITALIC_STYLE = {
+  fontFamily: DSA_FONT_FAMILY_READING,
+  fontSize: DSA_FONT_SIZE_L,
+  fontStyle: DSA_FONT_STYLE_ITALIC,
+  fontWeight: DSA_FONT_WEIGHT_BOLD,
+  letterSpacing: DSA_LETTER_SPACING_NORMAL,
+  lineHeight: DSA_LINE_HEIGHT_M,
+};
+
+/* Body (medium) */
+export const DSA_READING_BODY_M_NORMAL_STYLE = {
+  fontFamily: DSA_FONT_FAMILY_READING,
+  fontSize: DSA_FONT_SIZE_M,
+  fontWeight: DSA_FONT_WEIGHT_NORMAL,
+  letterSpacing: DSA_LETTER_SPACING_NORMAL,
+  lineHeight: DSA_LINE_HEIGHT_M,
+};
+
+export const DSA_READING_BODY_M_BOLD_STYLE = {
+  fontFamily: DSA_FONT_FAMILY_READING,
+  fontSize: DSA_FONT_SIZE_M,
+  fontWeight: DSA_FONT_WEIGHT_BOLD,
+  letterSpacing: DSA_LETTER_SPACING_NORMAL,
+  lineHeight: DSA_LINE_HEIGHT_M,
+};
+
+export const DSA_READING_BODY_M_ITALIC_STYLE = {
+  fontFamily: DSA_FONT_FAMILY_READING,
+  fontSize: DSA_FONT_SIZE_M,
+  fontStyle: DSA_FONT_STYLE_ITALIC,
+  fontWeight: DSA_FONT_WEIGHT_NORMAL,
+  letterSpacing: DSA_LETTER_SPACING_NORMAL,
+  lineHeight: DSA_LINE_HEIGHT_M,
+};
+
+export const DSA_READING_BODY_M_BOLDITALIC_STYLE = {
+  fontFamily: DSA_FONT_FAMILY_READING,
+  fontSize: DSA_FONT_SIZE_M,
+  fontStyle: DSA_FONT_STYLE_ITALIC,
+  fontWeight: DSA_FONT_WEIGHT_BOLD,
+  letterSpacing: DSA_LETTER_SPACING_NORMAL,
+  lineHeight: DSA_LINE_HEIGHT_M,
+};
+
+/* Body (small) */
+export const DSA_READING_BODY_S_NORMAL_STYLE = {
+  fontFamily: DSA_FONT_FAMILY_READING,
+  fontSize: DSA_FONT_SIZE_S,
+  fontWeight: DSA_FONT_WEIGHT_NORMAL,
+  letterSpacing: DSA_LETTER_SPACING_NORMAL,
+  lineHeight: DSA_LINE_HEIGHT_S,
+};
+
+export const DSA_READING_BODY_S_BOLD_STYLE = {
+  fontFamily: DSA_FONT_FAMILY_READING,
+  fontSize: DSA_FONT_SIZE_S,
+  fontWeight: DSA_FONT_WEIGHT_BOLD,
+  letterSpacing: DSA_LETTER_SPACING_NORMAL,
+  lineHeight: DSA_LINE_HEIGHT_S,
+};
+
+export const DSA_READING_BODY_S_ITALIC_STYLE = {
+  fontFamily: DSA_FONT_FAMILY_READING,
+  fontSize: DSA_FONT_SIZE_S,
+  fontStyle: DSA_FONT_STYLE_ITALIC,
+  fontWeight: DSA_FONT_WEIGHT_NORMAL,
+  letterSpacing: DSA_LETTER_SPACING_NORMAL,
+  lineHeight: DSA_LINE_HEIGHT_S,
+};
+
+export const DSA_READING_BODY_S_BOLDITALIC_STYLE = {
+  fontFamily: DSA_FONT_FAMILY_READING,
+  fontSize: DSA_FONT_SIZE_S,
+  fontStyle: DSA_FONT_STYLE_ITALIC,
+  fontWeight: DSA_FONT_WEIGHT_BOLD,
+  letterSpacing: DSA_LETTER_SPACING_NORMAL,
+  lineHeight: DSA_LINE_HEIGHT_S,
+};
+
+/* Body (extra small) */
+export const DSA_READING_BODY_XS_NORMAL_STYLE = {
+  fontFamily: DSA_FONT_FAMILY_READING,
+  fontSize: DSA_FONT_SIZE_XS,
+  fontWeight: DSA_FONT_WEIGHT_NORMAL,
+  letterSpacing: DSA_LETTER_SPACING_NORMAL,
+  lineHeight: DSA_LINE_HEIGHT_S,
+};
+
+export const DSA_READING_BODY_XS_BOLD_STYLE = {
+  fontFamily: DSA_FONT_FAMILY_READING,
+  fontSize: DSA_FONT_SIZE_XS,
+  fontWeight: DSA_FONT_WEIGHT_BOLD,
+  letterSpacing: DSA_LETTER_SPACING_NORMAL,
+  lineHeight: DSA_LINE_HEIGHT_S,
+};
+
+export const DSA_READING_BODY_XS_ITALIC_STYLE = {
+  fontFamily: DSA_FONT_FAMILY_READING,
+  fontSize: DSA_FONT_SIZE_XS,
+  fontStyle: DSA_FONT_STYLE_ITALIC,
+  fontWeight: DSA_FONT_WEIGHT_NORMAL,
+  letterSpacing: DSA_LETTER_SPACING_NORMAL,
+  lineHeight: DSA_LINE_HEIGHT_S,
+};
+
+export const DSA_READING_BODY_XS_BOLDITALIC_STYLE = {
+  fontFamily: DSA_FONT_FAMILY_READING,
+  fontSize: DSA_FONT_SIZE_XS,
+  fontStyle: DSA_FONT_STYLE_ITALIC,
+  fontWeight: DSA_FONT_WEIGHT_BOLD,
+  letterSpacing: DSA_LETTER_SPACING_NORMAL,
+  lineHeight: DSA_LINE_HEIGHT_S,
+};
