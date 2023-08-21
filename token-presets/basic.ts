1370d9265190b243b25732c0a6f4459a990f2cf5
gabrielguilhoto <gabriel.guilhoto@geekie.com.br>
2023-08-21 18:06:25 -0300

    Cria pré-definições de tipografia (#19)
    # Contexto
    Tínhamos criado os tokens de tipografia isolados (font family, font size, font weight etc.), mas queremos também uma forma de aplicar esses tokens em conjuntos como definidos no Figma. Mais detalhes em https://geekie.atlassian.net/browse/EOF-66.
    
    # Mudanças
    Defini objetos com as pré-definições de tipografia, me baseando no que está documentado no [Figma](https://www.figma.com/file/CGaJEpou7a3NO8N5KX7CSL/Design-System?type=design&node-id=3019-4896&mode=design&t=10ZQe3yA8BsDU8BB-4). Mais detalhes sobre a escolha desse formato no [Notion](https://www.notion.so/geekie/Design-system-pr-defini-es-de-tipografia-d87f97c14999448f88069ee6af69692e?pvs=4).
    
    Separei as pré-definições em 3 arquivos (`basic`, `ludic` e `reading`) de acordo com o tipo de fonte, agrupados numa pasta `presets`, e documentei como usar no readme.
    
    Ainda está pendente:
    * documentar essas pré-definições no Storybook
    * resolver warnings de TypeScript que acontecem ao usar os objetos numa stylesheet
    * permitir que as pré-definições sejam usadas num contexto sem polyfills do RN (pela diferença de formato do line height)
    
    # Como testar
    No SGLearner:
    * adicionei a lib do design system como uma dependência (colocando a URL do Git no `package.json` e rodando `yarn`
    * importei uma pré-definição numa tela que use polyfills do RN (peguei a `ProfileEditor`) e usei ela na definição de um estilo
    * vi que o estilo esperado aparecia na web

---
+++ token-presets/basic.ts
@@ -0,0 +1,195 @@
+import {
+  DSA_FONT_FAMILY_BASIC,
+  DSA_FONT_SIZE_L,
+  DSA_FONT_SIZE_M,
+  DSA_FONT_SIZE_S,
+  DSA_FONT_SIZE_XL,
+  DSA_FONT_SIZE_XS,
+  DSA_FONT_SIZE_XXL,
+  DSA_FONT_SIZE_XXS,
+  DSA_FONT_SIZE_XXXL,
+  DSA_FONT_STYLE_ITALIC,
+  DSA_FONT_WEIGHT_BOLD,
+  DSA_FONT_WEIGHT_NORMAL,
+  DSA_LETTER_SPACING_NORMAL,
+  DSA_LETTER_SPACING_WIDE,
+  DSA_LINE_HEIGHT_L,
+  DSA_LINE_HEIGHT_M,
+  DSA_LINE_HEIGHT_S,
+  DSA_LINE_HEIGHT_XL,
+  DSA_LINE_HEIGHT_XS,
+} from '../build/tokens/ts/tokens';
+
+/* Headings */
+export const DSA_BASIC_HEADING_H1_STYLE = {
+  fontFamily: DSA_FONT_FAMILY_BASIC,
+  fontSize: DSA_FONT_SIZE_XXXL,
+  fontWeight: DSA_FONT_WEIGHT_BOLD,
+  letterSpacing: DSA_LETTER_SPACING_WIDE,
+  lineHeight: DSA_LINE_HEIGHT_XL,
+};
+
+export const DSA_BASIC_HEADING_H2_STYLE = {
+  fontFamily: DSA_FONT_FAMILY_BASIC,
+  fontSize: DSA_FONT_SIZE_XXL,
+  fontWeight: DSA_FONT_WEIGHT_BOLD,
+  letterSpacing: DSA_LETTER_SPACING_NORMAL,
+  lineHeight: DSA_LINE_HEIGHT_L,
+};
+
+export const DSA_BASIC_HEADING_H3_STYLE = {
+  fontFamily: DSA_FONT_FAMILY_BASIC,
+  fontSize: DSA_FONT_SIZE_XL,
+  fontWeight: DSA_FONT_WEIGHT_BOLD,
+  letterSpacing: DSA_LETTER_SPACING_NORMAL,
+  lineHeight: DSA_LINE_HEIGHT_M,
+};
+
+/* Body (large) */
+export const DSA_BASIC_BODY_L_NORMAL_STYLE = {
+  fontFamily: DSA_FONT_FAMILY_BASIC,
+  fontSize: DSA_FONT_SIZE_L,
+  fontWeight: DSA_FONT_WEIGHT_NORMAL,
+  letterSpacing: DSA_LETTER_SPACING_NORMAL,
+  lineHeight: DSA_LINE_HEIGHT_M,
+};
+
+export const DSA_BASIC_BODY_L_BOLD_STYLE = {
+  fontFamily: DSA_FONT_FAMILY_BASIC,
+  fontSize: DSA_FONT_SIZE_L,
+  fontWeight: DSA_FONT_WEIGHT_BOLD,
+  letterSpacing: DSA_LETTER_SPACING_NORMAL,
+  lineHeight: DSA_LINE_HEIGHT_M,
+};
+
+export const DSA_BASIC_BODY_L_ITALIC_STYLE = {
+  fontFamily: DSA_FONT_FAMILY_BASIC,
+  fontSize: DSA_FONT_SIZE_L,
+  fontStyle: DSA_FONT_STYLE_ITALIC,
+  fontWeight: DSA_FONT_WEIGHT_NORMAL,
+  letterSpacing: DSA_LETTER_SPACING_NORMAL,
+  lineHeight: DSA_LINE_HEIGHT_M,
+};
+
+export const DSA_BASIC_BODY_L_BOLDITALIC_STYLE = {
+  fontFamily: DSA_FONT_FAMILY_BASIC,
+  fontSize: DSA_FONT_SIZE_L,
+  fontStyle: DSA_FONT_STYLE_ITALIC,
+  fontWeight: DSA_FONT_WEIGHT_BOLD,
+  letterSpacing: DSA_LETTER_SPACING_NORMAL,
+  lineHeight: DSA_LINE_HEIGHT_M,
+};
+
+/* Body (medium) */
+export const DSA_BASIC_BODY_M_NORMAL_STYLE = {
+  fontFamily: DSA_FONT_FAMILY_BASIC,
+  fontSize: DSA_FONT_SIZE_M,
+  fontWeight: DSA_FONT_WEIGHT_NORMAL,
+  letterSpacing: DSA_LETTER_SPACING_NORMAL,
+  lineHeight: DSA_LINE_HEIGHT_M,
+};
+
+export const DSA_BASIC_BODY_M_BOLD_STYLE = {
+  fontFamily: DSA_FONT_FAMILY_BASIC,
+  fontSize: DSA_FONT_SIZE_M,
+  fontWeight: DSA_FONT_WEIGHT_BOLD,
+  letterSpacing: DSA_LETTER_SPACING_NORMAL,
+  lineHeight: DSA_LINE_HEIGHT_M,
+};
+
+export const DSA_BASIC_BODY_M_ITALIC_STYLE = {
+  fontFamily: DSA_FONT_FAMILY_BASIC,
+  fontSize: DSA_FONT_SIZE_M,
+  fontStyle: DSA_FONT_STYLE_ITALIC,
+  fontWeight: DSA_FONT_WEIGHT_NORMAL,
+  letterSpacing: DSA_LETTER_SPACING_NORMAL,
+  lineHeight: DSA_LINE_HEIGHT_M,
+};
+
+export const DSA_BASIC_BODY_M_BOLDITALIC_STYLE = {
+  fontFamily: DSA_FONT_FAMILY_BASIC,
+  fontSize: DSA_FONT_SIZE_M,
+  fontStyle: DSA_FONT_STYLE_ITALIC,
+  fontWeight: DSA_FONT_WEIGHT_BOLD,
+  letterSpacing: DSA_LETTER_SPACING_NORMAL,
+  lineHeight: DSA_LINE_HEIGHT_M,
+};
+
+/* Body (small) */
+export const DSA_BASIC_BODY_S_NORMAL_STYLE = {
+  fontFamily: DSA_FONT_FAMILY_BASIC,
+  fontSize: DSA_FONT_SIZE_S,
+  fontWeight: DSA_FONT_WEIGHT_NORMAL,
+  letterSpacing: DSA_LETTER_SPACING_NORMAL,
+  lineHeight: DSA_LINE_HEIGHT_S,
+};
+
+export const DSA_BASIC_BODY_S_BOLD_STYLE = {
+  fontFamily: DSA_FONT_FAMILY_BASIC,
+  fontSize: DSA_FONT_SIZE_S,
+  fontWeight: DSA_FONT_WEIGHT_BOLD,
+  letterSpacing: DSA_LETTER_SPACING_NORMAL,
+  lineHeight: DSA_LINE_HEIGHT_S,
+};
+
+export const DSA_BASIC_BODY_S_ITALIC_STYLE = {
+  fontFamily: DSA_FONT_FAMILY_BASIC,
+  fontSize: DSA_FONT_SIZE_S,
+  fontStyle: DSA_FONT_STYLE_ITALIC,
+  fontWeight: DSA_FONT_WEIGHT_NORMAL,
+  letterSpacing: DSA_LETTER_SPACING_NORMAL,
+  lineHeight: DSA_LINE_HEIGHT_S,
+};
+
+export const DSA_BASIC_BODY_S_BOLDITALIC_STYLE = {
+  fontFamily: DSA_FONT_FAMILY_BASIC,
+  fontSize: DSA_FONT_SIZE_S,
+  fontStyle: DSA_FONT_STYLE_ITALIC,
+  fontWeight: DSA_FONT_WEIGHT_BOLD,
+  letterSpacing: DSA_LETTER_SPACING_NORMAL,
+  lineHeight: DSA_LINE_HEIGHT_S,
+};
+
+/* Body (extra small) */
+export const DSA_BASIC_BODY_XS_NORMAL_STYLE = {
+  fontFamily: DSA_FONT_FAMILY_BASIC,
+  fontSize: DSA_FONT_SIZE_XS,
+  fontWeight: DSA_FONT_WEIGHT_NORMAL,
+  letterSpacing: DSA_LETTER_SPACING_NORMAL,
+  lineHeight: DSA_LINE_HEIGHT_S,
+};
+
+export const DSA_BASIC_BODY_XS_BOLD_STYLE = {
+  fontFamily: DSA_FONT_FAMILY_BASIC,
+  fontSize: DSA_FONT_SIZE_XS,
+  fontWeight: DSA_FONT_WEIGHT_BOLD,
+  letterSpacing: DSA_LETTER_SPACING_NORMAL,
+  lineHeight: DSA_LINE_HEIGHT_S,
+};
+
+export const DSA_BASIC_BODY_XS_ITALIC_STYLE = {
+  fontFamily: DSA_FONT_FAMILY_BASIC,
+  fontSize: DSA_FONT_SIZE_XS,
+  fontStyle: DSA_FONT_STYLE_ITALIC,
+  fontWeight: DSA_FONT_WEIGHT_NORMAL,
+  letterSpacing: DSA_LETTER_SPACING_NORMAL,
+  lineHeight: DSA_LINE_HEIGHT_S,
+};
+
+export const DSA_BASIC_BODY_XS_BOLDITALIC_STYLE = {
+  fontFamily: DSA_FONT_FAMILY_BASIC,
+  fontSize: DSA_FONT_SIZE_XS,
+  fontStyle: DSA_FONT_STYLE_ITALIC,
+  fontWeight: DSA_FONT_WEIGHT_BOLD,
+  letterSpacing: DSA_LETTER_SPACING_NORMAL,
+  lineHeight: DSA_LINE_HEIGHT_S,
+};
+
+/* Body (extra extra small) */
+export const DSA_BASIC_BODY_XXS_NORMAL_STYLE = {
+  fontFamily: DSA_FONT_FAMILY_BASIC,
+  fontSize: DSA_FONT_SIZE_XXS,
+  fontWeight: DSA_FONT_WEIGHT_NORMAL,
+  letterSpacing: DSA_LETTER_SPACING_WIDE,
+  lineHeight: DSA_LINE_HEIGHT_XS,
+};
---
+++ token-presets/index.ts
@@ -0,0 +1,3 @@
+export * from './basic';
+export * from './ludic';
+export * from './reading';
