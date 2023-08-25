a88aec857d362dc4842ef5454914dd8797b9b234
Seu nome <leonardocpadua7@gmail.com>
2023-08-25 16:27:47 -0300

    fixes

--- helpers/webOnlyHelpers.ts
+++ helpers/webOnlyHelpers.ts
@@ -1,5 +1,3 @@
-import { type Preset } from '../token-presets';
-
 const toPx = (value: number | string): string => {
   return `${value}px`;
 };
@@ -10,6 +8,8 @@ export const toWebToken = (token: number): string => {
 };
 
 // Essa função foi criada para resolver o uso dos presets em casos web-only
-export const toWebPreset = (preset: Preset): Preset => {
+export const toWebPreset = <Preset extends { lineHeight: number }>(
+  preset: Preset
+): Preset & { lineHeight: string } => {
   return { ...preset, lineHeight: toPx(preset.lineHeight) };
 };
--- utils/PresetsDocBlock/index.tsx
+++ utils/PresetsDocBlock/index.tsx
@@ -4,11 +4,18 @@ import * as tokens from '../../built-tokens/js/tokens';
 import * as presets from '../../token-presets';
 
 import { toWebPreset } from '../../helpers/webOnlyHelpers';
-import { type Preset } from '../../token-presets';
 
 const allPresets = { ...presets };
 
 type PresetNames = keyof typeof allPresets;
+
+interface Preset {
+  fontFamily: string;
+  fontSize: number;
+  fontWeight: string;
+  letterSpacing: number;
+  lineHeight: number;
+}
 
 interface PresetsDocBlockProps {
   name: string;
@@ -23,35 +30,38 @@ const PresetsDocBlock: React.FC<PresetsDocBlockProps> = (props) => {
       <div className="name">
         {name}
         <div className="token-name">
-          {(Object.keys(allPresets) as PresetNames[]).map((key) => {
-            if (allPresets[key] === preset) return key;
+          {(Object.keys(allPresets) as PresetNames[]).map((presetName) => {
+            if (allPresets[presetName] === preset) return presetName;
             return null;
           })}
         </div>
       </div>
       <div className="tokens">
-        {(Object.keys(preset) as PresetNames[]).map((key) => {
-          return (
-            <div className="token" key={key}>
-              <div className="key">{key}:</div>
-              <div className="value">
-                {Object.keys(tokens).map((tokenKey) => {
-                  if (
-                    tokens[tokenKey as keyof typeof tokens] ===
-                      preset[key as keyof typeof preset] &&
-                    tokenKey
-                      .toLowerCase()
-                      .replace(/_/g, '')
-                      .includes(key.toLowerCase())
-                  ) {
-                    return tokenKey;
-                  }
-                  return null;
-                })}
+        {(Object.keys(preset) as Array<keyof typeof preset>).map(
+          (presetStyle) => {
+            return (
+              <div className="token" key={presetStyle}>
+                <div className="key">{presetStyle}:</div>
+                <div className="value">
+                  {(Object.keys(tokens) as Array<keyof typeof tokens>).map(
+                    (tokenName) => {
+                      if (
+                        tokens[tokenName] === preset[presetStyle] &&
+                        tokenName
+                          .toLowerCase()
+                          .replace(/_/g, '')
+                          .includes(presetStyle.toLowerCase())
+                      ) {
+                        return tokenName;
+                      }
+                      return null;
+                    }
+                  )}
+                </div>
               </div>
-            </div>
-          );
-        })}
+            );
+          }
+        )}
       </div>
       <div className="preview" style={toWebPreset(preset)}>
         almost before we knew it, we had left the ground.
