725f28ca07f7ff5a7e36bb4f1f36b520edafaeda
Leonardo Padua <leonardocpadua7@gmail.com>
2024-01-02 16:13:19 -0300

    remove built-tokens do ignore

---
+++ built-tokens/js/semantic-tokens.ts
@@ -0,0 +1,12 @@
+/**
+ * Do not edit directly
+ * Generated on Tue, 02 Jan 2024 18:47:09 GMT
+ */
+
+export const DSA_COLOR_BG_1 = "DSA_COLOR_BG_1";
+export const DSA_COLOR_BG_2 = "DSA_COLOR_BG_2";
+export const DSA_COLOR_TEXT_1 = "DSA_COLOR_TEXT_1";
+export const DSA_COLOR_TEXT_2 = "DSA_COLOR_TEXT_2";
+export const DSA_COLOR_TEXT_3 = "DSA_COLOR_TEXT_3";
+export const DSA_COLOR_BORDER_1 = "DSA_COLOR_BORDER_1";
+export const DSA_COLOR_BORDER_2 = "DSA_COLOR_BORDER_2";
