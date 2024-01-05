2e12f8d5565f6cf76bfe81f696631bb93dca0b38
Leonardo Pádua <leonardocpadua7@gmail.com>
2024-01-05 17:13:43 -0300

    add getThemeTokenValue (#34)

---
+++ themes/getThemeToken.ts
@@ -0,0 +1,30 @@
+import * as defaultTokens from '../built-tokens/js/semantic-tokens';
+import * as darkTokens from '../built-tokens/js/dark-tokens';
+import * as lightTokens from '../built-tokens/js/light-tokens';
+import type { ThemeType } from './themes';
+
+function getThemeToken(
+  theme: ThemeType,
+  tokenName: keyof typeof darkTokens
+): string {
+  if (theme === 'dark') {
+    return darkTokens[tokenName];
+  } else if (theme === 'light') {
+    return lightTokens[tokenName];
+  }
+
+  return defaultTokens[tokenName];
+}
+
+export const getThemeTokenValue = (token: string, theme: ThemeType): string => {
+  if (typeof token === 'string' && token.includes('DSA_COLOR')) {
+    const regexpSize = /(DSA_COLOR_TEXT_|DSA_COLOR_BG_|DSA_COLOR_BORDER_)\w+/;
+    const match = token.match(regexpSize);
+    if (match !== null && match?.length > 0) {
+      const value = getThemeToken(theme, match[0] as keyof typeof darkTokens);
+      const tokenWithValue = token.replace(match[0], value);
+      return tokenWithValue;
+    }
+  }
+  return token;
+};
--- tokens.ts
+++ tokens.ts
@@ -2,3 +2,4 @@ export * from './built-tokens/js/tokens';
 export * from './built-tokens/js/semantic-tokens';
 
 export * from './helpers/webOnlyHelpers';
+export * from './themes/getThemeToken';
