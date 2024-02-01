e1ef1e1f53ab72745a74c8135e082a3e7f2e25dd
Leonardo Pádua <leonardocpadua7@gmail.com>
2024-02-01 16:33:13 -0300

    Setup do Jest e react testing library (#37)
    * setup do jest
    
    * ajustes

---
+++ jest.config.ts
@@ -0,0 +1,14 @@
+/**
+ * For a detailed explanation regarding each configuration property, visit:
+ * https://jestjs.io/docs/configuration
+ */
+
+import type { Config } from 'jest';
+
+const config: Config = {
+  clearMocks: true,
+  modulePathIgnorePatterns: ['/node_modules/', '/build/'],
+  testRegex: '(__tests__/.*\\.)?test\\.(js|ts|tsx)$',
+};
+
+export default config;
