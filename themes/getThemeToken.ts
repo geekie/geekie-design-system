bcdf9cefa35a5476a0d9327abd9cb8980122e982
Leonardo Padua <leonardocpadua7@gmail.com>
2024-02-22 14:49:13 -0300

    wip poc

--- themes/getThemeToken.ts
+++ themes/getThemeToken.ts
@@ -18,7 +18,7 @@ function getThemeToken(
 
 export const getThemeTokenValue = (token: string, theme: ThemeType): string => {
   if (typeof token === 'string' && token.includes('DSA_COLOR')) {
-    const regexpSize = /(DSA_COLOR_TEXT_|DSA_COLOR_BG_|DSA_COLOR_BORDER_)\w+/;
+    const regexpSize = /(DSA_COLOR_TEXT_|DSA_COLOR_BG_|DSA_COLOR_BORDER_|DSA_COLOR_SUBJECT_)\w+/;
     const match = token.match(regexpSize);
     if (match !== null && match?.length > 0) {
       const value = getThemeToken(theme, match[0] as keyof typeof darkTokens);
